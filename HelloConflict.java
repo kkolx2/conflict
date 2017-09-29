e685bd113929ccba09b5d792ec4622d818d97dea
java@guestBig <kkolx2@nate.com>
2017-09-29 11:19:44 +0900

    11

--- HelloConflict.java
+++ HelloConflict.java
@@ -1,7 +1,15 @@
 
 public class HelloConflict {
 	public static void main(String[] args){
-		
+	
+	int sum=0;
+	int i=0;
+	int max=100;
+	while(max <100){
+		i++	
+		sum += i;
+		max++;
+	}		
 
 	}	
 }
